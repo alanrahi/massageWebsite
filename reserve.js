11635e6600228c7349a9633bd9b19184460ec8f9
jeanette park <jpark@efn.org>
2014-11-08 09:28:31 -0800

    Reserve button edits with calendar image

--- reserve.js
+++ reserve.js
@@ -1,4 +1,24 @@
 
-$('button[id="reserve"]').click(function(){
+$('button').click(function(){
   alert("Calendar");
   })
+
+/*var inter;
+
+$(document).ready(function(){
+    inter  = setInterval(function(){
+        $('.calendar').append('<img src="images/calendar"onclick="myFunction(this)">');
+    },1000);
+});
+
+var counter = 0;
+
+function myFunction(img) {
+    counter++;
+    document.getElementById('countervalue').innerHTML = counter;
+    img.onclick = null;
+    img.remove();
+    if(counter === 20){
+        clearInterval(inter);
+    }
+}*/
